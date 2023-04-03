4f9452830dd087e034b92bee1522efbcf250a1b7
Bruna <brunadias_30@hotmail.com>
2023-04-03 07:46:09 -0300

    log axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import axios from 'axios';
+
 function App() {
+  const url = 'http://viacep.com.br/ws/05402918/json'
+
+  useEffect(() => {
+    axios.get(url)
+    .then((r) => {
+      console.log(r)
+    })
+  }, [])
+
   return (
     <div className="App">
       oi
