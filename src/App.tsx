a9a6e42017df8966f46b062ba92163587ed529c4
Bruna <brunadias_30@hotmail.com>
2023-04-03 08:24:49 -0300

    using axios api

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import axios from 'axios';
 
-function App() {
-  const url = 'http://viacep.com.br/ws/05402918/json'
+interface Cep {
+  bairro: string;
+  cep: string;
+  complemento:string;
+  ddd:string;
+  gia: string;
+  ibge: string;
+  localidade: string;
+  logradouro:string;
+  siafi: string;
+  uf:string;
+}
 
-  useEffect(() => {
+function App() {
+  const [cep, setCep] = useState('')
+  const [response, setResponse] = useState({} as Cep);
+  const url = `http://viacep.com.br/ws/${cep}/json`
+
+  function obter() {
     axios.get(url)
-    .then((r) => {
-      console.log(r)
-    })
-  }, [])
+      .then(({ data }) => {
+        console.log(data)
+        setResponse(data);
+      })
+  }
 
   return (
     <div className="App">
-      oi
+      <div style={{ alignItems: 'center', margin: '5px', padding: '5px', background: '#F5FFFA', textAlign: 'center' }}>
+        <p><label htmlFor="cep">CEP</label></p>
+        <input id='cep' value={cep} onChange={(e) => setCep(e.target.value)} />
+        <button onClick={() => obter()}>Enviar</button>
+      </div>
+      <div style={{background:'#FFF0F5'}}>
+        <p>Bairro: {response.bairro}</p>
+        <p>Localidade: {response.localidade}</p>
+      </div>
     </div>
   );
 }
